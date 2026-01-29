899166d625157e7b9f7b7a9665aa6653a2875a3e
vinnimous <ron.valenzuela81@gmail.com>
2026-01-29 17:54:30 -0500

    chore: update Gradle and plugin versions in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
 
 tasks.register("clean", Delete::class) {
