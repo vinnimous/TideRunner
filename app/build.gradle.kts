cbc93a297894030e0237826242ec75fcb81a70e3
vinnimous <ron.valenzuela81@gmail.com>
2026-02-23 16:32:18 -0500

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.fishing.conditions"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
